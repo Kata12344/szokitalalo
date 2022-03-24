f2e78d75fa8f8f6d99a175614981140b1f6ad4f7
rohacsidana <rohacsi.dana@gmail.com>
2022-03-24 13:16:28 +0100

    feladat kiiras metodus megirva

--- szoKitalalo/src/szokitalalo/SzoKitalalo.java
+++ szoKitalalo/src/szokitalalo/SzoKitalalo.java
@@ -6,11 +6,24 @@ public class SzoKitalalo {
 
     static int[] szoTomb;
     static String kitalalandoSzo = "";
-    static Scanner sc = new Scanner(System.in);
+
     static String bekertszo = "";
     static int talalatokSzama = 0;
 
     public static void main(String[] args) {
+
+    }
+
+    private static void feladatKiiras() {
+        System.out.println("Üdvözöllek! Ez itt egy - Kata, Kriszti és Dana által készített szókitalálós játék. A gép gondol egy 2 betűs szóra, és Neked ezt kell majd kitalálnod. Írnod kell 2 betűs szavakat, a gép pedig közölni fogja veled, hogy milyen arányban egyezik az, a kitalálandő szóval.");
+    }
+
+    private static void beker() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Tipp: ");
+        do {
+            bekertszo = sc.nextLine();
+        } while (bekertszo.length() != 2);
 
     }
 
